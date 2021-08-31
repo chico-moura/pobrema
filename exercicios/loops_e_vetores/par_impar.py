3f001f2bc8878fed1930b9bbf11f693cf236bcf6
chico <chico.zimcolor@gmail.com>
2021-08-31 16:36:32 -0300

    exemple exercise created

---
+++ exercicios/loops_e_vetores/par_impar.py
@@ -0,0 +1,24 @@
+"""
+
+Escreva as duas funções:
+    selecionar_pares()
+    selecionar_impares()
+    
+Cada uma delas rececbe como argumento um vetor de inteiro.
+Cada uma deve retornar um vetor de inteiro.
+
+Tente não se repetir
+
+"""
+
+
+def selecionar_pares(numeros: [int]) -> [int]:
+    # escreva aqui
+    pass
+
+
+def selecionar_impares(numeros: [int]) -> [int]:
+    # escreva aqui
+    pass
+
+# Pode declarar funções auxiliares abaixo, mas não mude o nome das funções acima
