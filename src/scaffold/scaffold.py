b5b636bd2e03e8d0e22170267f439a29d5df7f70
chico <chico.zimcolor@gmail.com>
2021-09-05 11:42:04 -0300

    Scaffold remade

--- src/scaffold/scaffold.py
+++ src/scaffold/scaffold.py
@@ -1,87 +1,62 @@
-import os
 import sys
 
-from src.enums.file_system_enum import FileSystemEnum
-from src.scaffold.tag_set import Tags
-from src.scaffold.path_repository.path_repository import PathRepo, BasicPath
+from src.scaffold.path_repository.models import Dir
+from src.enums import FileSystemEnum
+from src.scaffold.path_repository.path_repository import PathRepo
+from src.scaffold.template_repository.template_repository import TemplateRepo
 
 
 class Scaffold:
     __group_name: str
     __problem_name: str
-    __path: PathRepo
-    __tags: Tags
+    __paths: PathRepo
+    __templates: TemplateRepo
 
     def __init__(self, group: str, problem: str) -> None:
         self.__group_name = group
         self.__problem_name = problem
-        self.__path = PathRepo(group, problem)
-        self.__tags = Tags(problem)
+        self.__paths = PathRepo(group, problem)
+        self.__templates = TemplateRepo(problem_name=problem)
 
     def create(self) -> None:
         try:
             self._validate_arguments()
-            self._initialize_dirs()
-            self._initialize_files()
+            self._create_structure()
 
         except FileExistsError or AttributeError as e:
             print(e)
 
     def _validate_arguments(self) -> None:
-        existing_files = [file for file in self.__path.unique_paths if file.exists]
+        existing_files = [file for file in self.__paths.unique_paths if file.exists]
         if existing_files:
             existing_files_to_string = '\n'.join([path.path() for path in existing_files])
             raise FileExistsError(f'Files or directories already exists:\n{existing_files_to_string}')
 
-    def _initialize_dirs(self) -> None:
-        self._create_dir(self.__path.dir.problem_group)
-        self._create_package(self.__path.file.problem, self.__path.file.init_problem)
-        self._create_file(self.__path.key_file)
-
-    def _initialize_files(self) -> None:
-        self._create_file(
-            path=self.__path.problem_file,
-            template=PathConstants.PROBLEM_TEMPLATE
+    def _create_structure(self) -> None:
+        group_dir = Dir(
+            path=self.__paths.dir.problem_group.path,
+            accept_existing=True
         )
-        self._create_file(
-            path=self.__path.lock_file,
-            template=PathConstants.LOCK_TEMPLATE
+        problem_dir = group_dir.spawn_dir(
+            name=self.__problem_name,
+            init_content=self.__templates.problem_init
         )
-        self._create_file(
-            path=self.__path.key_file,
-            template=PathConstants.KEY_TEMPLATE
+        problem_dir.spawn_file(
+            name=self.__problem_name,
+            content=self.__templates.problem
         )
-
-    @staticmethod
-    def _create_dir(path: BasicPath) -> None:
-        if not path.exists:
-            os.mkdir(path.path)
-        else:
-            raise FileExistsError(path.path)
-
-    def _create_package(self, path: BasicPath, init_template: BasicPath = None) -> None:
-        try:
-            self._create_dir(path)
-            init_path = path.with_complement(FileSystemEnum.INIT_FILE)
-            self._create_file(init_path, init_template)
-
-        except FileExistsError as e:
-            print(e)
-
-    def _create_file(self, path: BasicPath, template: BasicPath = None) -> None:
-        if path.exists:
-            raise FileExistsError(f'{path} already exists')
-
-        content = ''
-        if template:
-            content = self._get_content(template)
-        with open(path.path, 'w') as file:
-            file.write(content)
-
-    def _get_content(self, path: BasicPath) -> str:
-        with open(path.path, 'r') as file:
-            content = file.read()
-        return self.__tags.replace(content)
+        key_dir = problem_dir.spawn_dir(
+            name=FileSystemEnum.KEY,
+            init_content=self.__templates.key_init
+        )
+        key_dir.spawn_file(
+            name=FileSystemEnum.KEY,
+            content=self.__templates.key
+        )
+        key_dir.spawn_file(
+            name=FileSystemEnum.LOCK,
+            content=self.__templates.lock
+        )
 
 
 if __name__ == '__main__':
