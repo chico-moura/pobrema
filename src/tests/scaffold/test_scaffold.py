b9a18da876505a8aaa1633492b8f2e34b8b63a11
chico <chico.zimcolor@gmail.com>
2021-09-05 14:39:18 -0300

    Scaffold test fix

--- src/tests/scaffold/test_scaffold.py
+++ src/tests/scaffold/test_scaffold.py
@@ -4,22 +4,22 @@ from unittest import TestCase
 
 from src.scaffold import Scaffold
 from src.enums import FileSystemEnum as En
+from src.enums import PathConstants
 
 
 class TestScaffold(TestCase):
     def setUp(self) -> None:
-        self.group = 'foo'
-        self.problem = 'bar'
-        self.scaffold = Scaffold(self.group, self.problem)
+        self.group_name = 'foo'
+        self.problem_name = 'bar'
+        self.group_path = f'{PathConstants.PROBLEM_GROUPS}/{self.group_name}'
+        self.problem_path = f'{self.group_path}/{self.problem_name}/{self.problem_name}.{En.FILE_EXTENSION}'
+        self.scaffold = Scaffold(self.group_name, self.problem_name)
 
     def tearDown(self) -> None:
-        if Path(self.group).exists():
-            rmtree(self.group)
-
+        if Path(self.group_path).exists():
+            rmtree(self.group_path)
+            
     def test_scaffold_WHEN_nothing_exists_THEN_creates_structure(self) -> None:
-        expected_file = f'{self.group}/{self.problem}/{En.KEY}/{En.KEY}.{En.FILE_EXTENSION}'
-        print(expected_file)
-
         self.scaffold.create()
 
-        self.assertTrue(Path(expected_file).is_file())
+        self.assertTrue(Path(self.problem_path).is_file())
